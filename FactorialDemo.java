aab4fe46067af33d8e79142dd0bbf6ff8e92d622
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 15:26:55 +0400

    Test the method with values like 1, 2, and 3  +  Display the results using System.out.println()

--- FactorialDemo.java
+++ FactorialDemo.java
@@ -4,8 +4,16 @@ public class FactorialDemo {
 
     public static void main(String[]args){
 
+        // Create an object of Factorial class
 
         Factorial factorial = new Factorial();
-        factorial.factorial(5);
+
+        // Test the factorial method with different values
+
+        System.out.println("Factorial of 1 is :" +factorial.factorial(1));
+        System.out.println("Factorial of 2 is :" +factorial.factorial(2));
+        System.out.println("Factorial of 3 is :" +factorial.factorial(3));
+
+
     }
 }
