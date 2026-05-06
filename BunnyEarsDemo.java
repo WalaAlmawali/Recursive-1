3f6567b1d8700a73106156eb8c40a071af8b2113
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 15:42:39 +0400

    Call the bunnyEars2() method using the object + Test the method with values like 0, 1, and 2

--- BunnyEarsDemo.java
+++ BunnyEarsDemo.java
@@ -7,6 +7,11 @@ public class BunnyEarsDemo {
         // Create an object of BunnyEars class
         BunnyEars bunnyEars = new BunnyEars();
 
+        // Test the method with different values
+
+        System.out.println(bunnyEars.bunnyEars2(0));
+        System.out.println(bunnyEars.bunnyEars2(1));
+        System.out.println(bunnyEars.bunnyEars2(2));
 
 
 
