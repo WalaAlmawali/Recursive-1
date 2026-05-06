9a69b6d563de78dce6ea45c742cc9f9a8f26378b
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 16:02:43 +0400

    Call the count7() method using the object + Test the method with values like 717, 7, and 123

--- Count7Demo.java
+++ Count7Demo.java
@@ -6,6 +6,12 @@ public class Count7Demo {
         // Create an object of the Count7 class
         Count7 count7 = new Count7();
 
+        // Test the method with different values
+        System.out.println(count7.count7(717));
+        System.out.println(count7.count7(7));
+        System.out.println(count7.count7(123));
+
+
 
 
     }
