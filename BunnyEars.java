3dee0348fcc189011210c9fad6b4b3b3dad18d37
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 15:36:45 +0400

    Add Otherwise for the previous if condetion

--- BunnyEars.java
+++ BunnyEars.java
@@ -12,6 +12,9 @@ public class BunnyEars {
         // Check if the current bunny number is even
         if(n % 2 == 0){
             return 3 +bunnyEars2(n - 1);
+        }else{
+
+            return 2 + bunnyEars2(n - 1);
         }
 
     }
