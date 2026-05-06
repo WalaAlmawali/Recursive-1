7f334d2369dfd704a67047a870ee35067a125a47
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 16:39:49 +0400

    Test the method with arrays like [1, 2, 11], [11, 11], and [1, 2, 3, 4]

--- Array11Demo.java
+++ Array11Demo.java
@@ -6,6 +6,12 @@ public class Array11Demo {
         //Create an object of the Array11 class
         Array11 array11 = new Array11();
 
+        // Test arrays
+        int[] arr1 = {1, 2, 11};
+        int[] arr2 = {11, 11};
+        int[] arr3 = {1, 2, 3, 4};
+
+
     }
 
 
