af4b040422b06ad028a57f0453663983f7a2759c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 16:35:36 +0400

    Add Otherwise for the previous if condetion

--- Array11.java
+++ Array11.java
@@ -11,6 +11,8 @@ public class Array11 {
         //Check the current element equals to 11
         if(nums[index] == 11){
             return 1 + array11(nums, index + 1);
+        }else {
+            return array11(nums, index + 1);
         }
 
     }
