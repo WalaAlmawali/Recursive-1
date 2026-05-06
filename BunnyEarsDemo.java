e8a54f446b1e8f1d87a9204bbb82e9851976d272
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-06 15:39:55 +0400

    Create an object of the BunnyEars class

--- BunnyEarsDemo.java
+++ BunnyEarsDemo.java
@@ -4,5 +4,11 @@ public class BunnyEarsDemo {
     public static void main(String[] args){
 
 
+        // Create an object of BunnyEars class
+        BunnyEars bunnyEars = new BunnyEars();
+
+
+
+
     }
 }
