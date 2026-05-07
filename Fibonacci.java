402857668ee89fc6f8495be5a853c6028e244555
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:46:53 +0400

    Inside the method, define the base cases

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,7 +4,14 @@ public class Fibonacci {
     public int  fibonacci(int n){
 
 
+        //Base case
+        if(n == 0){
+            return 0;
+        }
 
+        if(n == 1){
+            return 1;
+        }
 
     }
 
