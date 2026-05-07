b81df5474e025f6c2b82aa082d0e6ee22a6eb4f8
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 10:27:22 +0400

    Call the endX() method using the object +  Test the method with strings+ Display the results

--- EndXDemo.java
+++ EndXDemo.java
@@ -8,6 +8,11 @@ public class EndXDemo {
         EndX endX = new EndX();
 
 
+        // Test the method with strings like "xxre", "xxhixx", and "xhixhix"
+        System.out.println(endX.endX("xxre"));
+        System.out.println(endX.endX("xxhixx"));
+        System.out.println(endX.endX("xhixhix"));
+
 
     }
 
