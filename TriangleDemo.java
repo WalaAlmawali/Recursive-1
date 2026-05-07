ff3f04032cc9c63342fc119f590d533f3953c6eb
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 09:35:15 +0400

    Call the triangle() method using the object  + Display the results using System.out.println()

--- TriangleDemo.java
+++ TriangleDemo.java
@@ -6,6 +6,12 @@ public class TriangleDemo {
         // Create an object of the Triangle class
         Triangle triangle = new Triangle();
 
+        // Test the method with values like 0, 1, and 2
+        System.out.println(triangle.triangle(0));
+        System.out.println(triangle.triangle(1));
+        System.out.println(triangle.triangle(2));
+
+
 
     }
 
