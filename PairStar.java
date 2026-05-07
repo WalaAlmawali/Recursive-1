f726add57f9727da7cb795a3ed5b16ece84ddcf6
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:13:47 +0400

    Check if the the first character equals to the second character

--- PairStar.java
+++ PairStar.java
@@ -8,5 +8,9 @@ public class PairStar {
             return str;
         }
 
+        if(str.charAt(0) == str.charAt(1)){
+            return str.charAt(0) + "*" + str.charAt(1);
+        }
+
     }
 }
