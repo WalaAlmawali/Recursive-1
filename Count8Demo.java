8191df9c75612320cc9dc5ec9415be1c9009e22e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 09:49:38 +0400

    Call the count8() method using the object +  Test the method +  Display the results

--- Count8Demo.java
+++ Count8Demo.java
@@ -5,6 +5,12 @@ public class Count8Demo {
         //  Create an object of the Count8 class
         Count8 count8 = new Count8();
 
+        // Test the method with values like 8, 818, and 8818
+        System.out.println(count8.count8(8));
+        System.out.println(count8.count8(818));
+        System.out.println(count8.count8(8818));
+
+
 
     }
 
