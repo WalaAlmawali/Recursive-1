01f804a6f370d3c12a33bae9e83e4d2402ad0f7d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:40:30 +0400

    Inside the method, define the base case

--- StrCount.java
+++ StrCount.java
@@ -3,6 +3,10 @@ public class StrCount {
 
     public int  strCount(String str, String sub){
 
+        // Base case
+        if(str.length() < sub.length()){
+            return 0;
+        }
 
     }
 
