ff88be1a2f90bea4f176cbc1400d4102054cbaf5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:45:54 +0400

    Create an object of the StrCount class

--- StrCountDemo.java
+++ StrCountDemo.java
@@ -3,7 +3,8 @@ public class StrCountDemo {
 
     public static void main(String[] args){
 
-
+        //Create an object of the StrCount class
+        StrCount strCount = new StrCount();
     }
 
 
