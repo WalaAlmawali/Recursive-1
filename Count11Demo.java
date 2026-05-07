2488052a839a8c9dec6da36dac30a6f87a2f2fbb
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 10:58:19 +0400

    Call the count11() method using the object+  Display the results

--- Count11Demo.java
+++ Count11Demo.java
@@ -7,6 +7,12 @@ public class Count11Demo {
         //Create an object of the Count11 class
         Count11 count11 = new Count11();
 
+        // Test the method with strings like "11abc11", "abc11x11x11", and "111"
+        System.out.println(count11.count11("11abc11"));
+        System.out.println(count11.count11("abc11x11x11"));
+        System.out.println(count11.count11("111"));
+
+
 
     }
 
