a83acaafc83500ef1a2d8db4c3aabdfc1357a5bf
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:01:45 +0400

    Inside the method, define the base case

--- ParenBit.java
+++ ParenBit.java
@@ -3,6 +3,10 @@ public class ParenBit {
 
     public String  parenBit(String str){
 
+        //Base case
+        if(str.startsWith("(") && str.endsWith(")")){
+            return str;
+        }
 
 
     }
