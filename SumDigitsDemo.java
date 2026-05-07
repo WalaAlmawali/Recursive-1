f75c849736f31d3244740e1b892f04d625339efa
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 12:00:58 +0400

    Call the sumDigits() method using the object +  Test the method+ Display the results

--- SumDigitsDemo.java
+++ SumDigitsDemo.java
@@ -6,6 +6,12 @@ public class SumDigitsDemo {
         // Create an object of the SumDigits class
         SumDigits sumDigits = new SumDigits();
 
+        //Test the method with values like 126, 49, and 12
+        System.out.println(sumDigits.sumDigits(126));
+        System.out.println(sumDigits.sumDigits(49));
+        System.out.println(sumDigits.sumDigits(12));
+
+
 
 
     }
