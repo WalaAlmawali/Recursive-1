8c552735c8e1d6eacaf0d21154e2ffbcbc584608
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:21:53 +0400

    Create an object of the PairStar class

--- PairStarDemo.java
+++ PairStarDemo.java
@@ -2,7 +2,8 @@
 public class PairStarDemo {
     public static void main(String[] args){
 
-
+        //Create an object of the PairStar class
+        PairStar pairStar = new PairStar();
 
 
     }
