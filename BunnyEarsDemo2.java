9c7b6ec5457ab58afba92803452f2ba621a15dbd
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 09:00:42 +0400

    Test the method with values like 0, 1, and 2

--- BunnyEarsDemo2.java
+++ BunnyEarsDemo2.java
@@ -8,5 +8,11 @@ public class BunnyEarsDemo2 {
         // Create an object of the BunnyEars2 class
         BunnyEars2 bunnyEars2 = new BunnyEars2();
 
+        // Test the method with values like 0, 1, and 2
+        System.out.println(bunnyEars2.bunnyEars(0));
+        System.out.println(bunnyEars2.bunnyEars(1));
+        System.out.println(bunnyEars2.bunnyEars(2));
+
+
     }
 }
