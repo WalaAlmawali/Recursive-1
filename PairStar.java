7b9b71a58e5686b488fd93fdf1e6b588216cd531
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:08:57 +0400

    Inside the method, define the base case

--- PairStar.java
+++ PairStar.java
@@ -3,6 +3,10 @@ public class PairStar {
 
     public String pairStar(String str){
 
+        //Base case
+        if(str.length() == 0 || str.length() == 1){
+            return str;
+        }
 
     }
 }
