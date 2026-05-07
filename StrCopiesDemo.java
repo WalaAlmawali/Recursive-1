a9527f6a157bfbec0cfc48a200c1bb9d9d0f1290
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:37:15 +0400

    Create an object of the StrCopies class

--- StrCopiesDemo.java
+++ StrCopiesDemo.java
@@ -1,5 +1,14 @@
 
 public class StrCopiesDemo {
+    public static void main(String[] args){
+
+        // Create an object of the StrCopies class
+        StrCopies strCopies = new StrCopies();
 
 
-}
+
+
+    }
+
+
+    }
