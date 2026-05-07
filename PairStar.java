c8c4603ea4fd5b4fbf3a088bfa6a3cd493a8746a
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:17:00 +0400

    Add Otherwise for the previous if condetion

--- PairStar.java
+++ PairStar.java
@@ -9,7 +9,9 @@ public class PairStar {
         }
 
         if(str.charAt(0) == str.charAt(1)){
-            return str.charAt(0) + "*" + str.charAt(1);
+            return str.charAt(0) + "*" + pairStar(str.substring(1));
+        }else{
+            return str.charAt(0) + pairStar(str.substring(1));
         }
 
     }
