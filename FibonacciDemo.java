effd22a5661b5f30b953bdba4e7717f481dbafab
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:49:48 +0400

    Create an object of the Fibonacci class

--- FibonacciDemo.java
+++ FibonacciDemo.java
@@ -3,6 +3,8 @@ public class FibonacciDemo {
 
     public static void main(String[] args){
 
+        //Create an object of the Fibonacci class
+        Fibonacci fibonacci = new Fibonacci();
 
 
     }
