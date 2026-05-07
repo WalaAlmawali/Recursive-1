3e7df35de6953b61e6c62ff08922ba67f306a556
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 10:55:07 +0400

    Add otherwise for the previous if condetion

--- Count11.java
+++ Count11.java
@@ -12,6 +12,8 @@ public class Count11 {
         if(str.substring(0,2).equals("11")){
             return 1 + count11(str.substring(2));
 
+        }else{
+            return count11(str.substring(1));
         }
 
     }
