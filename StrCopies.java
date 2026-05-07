f517f0ea3fb0b9dcd6cbc0e46405aebea21c013e
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:31:40 +0400

    Check if the first part of the string matches sub

--- StrCopies.java
+++ StrCopies.java
@@ -8,10 +8,15 @@ public class StrCopies {
             return true;
         }
 
+        //Check if the length of str is less than the length of sub
         if(str.length() < sub.length()){
             return false;
         }
 
+        //Check if the first part of the string matches sub
+        if(str.substring(0,sub.length())== sub){
+            return strCopies(str.substring(1), sub, n - 1);
+        }
 
     }
 }
