15470a33ace5d04b5ce16baa3bd8a9c653e95626
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 08:37:44 +0400

    Call the countAbc() method using the object + Test the method with strings like abc, abcxxabc, and abaxxaba + Display the results using System.out.println()

--- CountAbcDemo.java
+++ CountAbcDemo.java
@@ -7,6 +7,12 @@ public static void main(String[] args){
     //Create an object of the CountAbc class
     CountAbc countAbc = new CountAbc();
 
+    // Test the method with different strings
+    System.out.println(countAbc.countAbc("abc"));
+    System.out.println(countAbc.countAbc("abcxxabc"));
+    System.out.println(countAbc.countAbc("abaxxaba"));
+
+
 }
 
 
