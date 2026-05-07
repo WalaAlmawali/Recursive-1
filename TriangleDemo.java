e9b708d53e8db99f7b358b753be982706f8a9669
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 09:31:48 +0400

    Create an object of the Triangle class

--- TriangleDemo.java
+++ TriangleDemo.java
@@ -3,6 +3,8 @@ public class TriangleDemo {
 
     public static void main(String[] args){
 
+        // Create an object of the Triangle class
+        Triangle triangle = new Triangle();
 
 
     }
