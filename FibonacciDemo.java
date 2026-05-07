f00d56f48349bbb4a61626b7ea3fa0eadd4aab0c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 11:51:37 +0400

    Call the fibonacci() method using the object+  Test the method+ Display the results

--- FibonacciDemo.java
+++ FibonacciDemo.java
@@ -7,6 +7,16 @@ public class FibonacciDemo {
         Fibonacci fibonacci = new Fibonacci();
 
 
+        // Test the method with values like 0, 1, 2, 5, and 10
+        System.out.println(fibonacci.fibonacci(0));
+        System.out.println(fibonacci.fibonacci(1));
+        System.out.println(fibonacci.fibonacci(2));
+        System.out.println(fibonacci.fibonacci(5));
+        System.out.println(fibonacci.fibonacci(10));
+
+
+
+
     }
 
 
