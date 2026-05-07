43fa7ea056ce7e4689f6bdd9b2be479ebe1f7ced
Wala AL mawali <wala.salam.9888@gmail.com>
2026-05-07 10:23:25 +0400

    Add otherwise part of the previous if condetion

--- EndX.java
+++ EndX.java
@@ -11,6 +11,8 @@ public class EndX {
         //Check if the first character is x
         if(str.charAt(0) == 'x'){
             return endX(str.substring(1)) + str.charAt(0);
+        } else{
+            return str.charAt(0) + endX(str.substring(1));
         }
 
     }
